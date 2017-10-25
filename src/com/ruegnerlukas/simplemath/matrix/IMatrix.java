55e6fc7f6de38e9ae9dfcebd1f9e462dcb19d00b
Lukas Rügner <ruegnerlukas@gmail.com>
2017-10-26 00:10:18 +0200

    Add matrix operations

--- src/com/ruegnerlukas/simplemath/matrix/IMatrix.java
+++ src/com/ruegnerlukas/simplemath/matrix/IMatrix.java
@@ -1,6 +1,6 @@
 package com.ruegnerlukas.simplemath.matrix;
 
-
+import com.ruegnerlukas.simplemath.vectors.IVector;
 
 public interface IMatrix {
 
@@ -13,8 +13,19 @@ public interface IMatrix {
 	public float[][] getData();
 	public float getData(int i, int j);
 	public void getRow(int row, float[] dest);
-	public void getColum(int column, float[] dest);
+	public void getColumn(int column, float[] dest);
+	public float[] getRow(int row);
+	public float[] getColumn(int column);
 	
+	public IVector getRowVector(int row);
+	public IVector getColumnVector(int column);
+	
+	
+	public void getSubmatrix(int c0, int r0, int c1, int r1, IMatrix dest);
+	public IMatrix getSubmatrix(int c0, int r0, int c1, int r1);
+
+	public IMatrix copyData(float[][] data);
+	public IMatrix setData(float[][] data);
 	public IMatrix setData(int i, int j, float v);
 	public IMatrix setDataCol(int column, float[] data);
 	public IMatrix setDataRow(int row, float[] data);
@@ -22,21 +33,27 @@ public interface IMatrix {
 	public IMatrix switchColumns(int c0, int c1);
 	public IMatrix switchRows(int r0, int r1);
 
+	public boolean isSquare();
+	public boolean compare(IMatrix mat);
+	public boolean compareSize(IMatrix mat);
+
+	public IMatrix transpose();
 	
-	/*
+	public IMatrix add(IMatrix mat);
+	public IMatrix sub(IMatrix mat);
 	
-	add matrix a to b
-	subtract matrix b from a
-	multiply matrix a by matrix b
-	mutiply matrix by scalar value
-	transpose matrix
-	invert matrix
-	get minor of matrix
-	calculate determinant
-	calulate rank
-	...
+	public IMatrix scale(float scalar);
+	public IMatrix mul(IMatrix mat);
+	public IMatrix mul(IMatrix mat, IMatrix dest);
+
+	public String toFormattedString();
 	
+	public IMatrix toUpperTriangle();
+	public boolean isUpperTriangular();
+
+	public float determinant();
+	public float trace();
 	
-	*/
+	public IMatrix copy();
 	
 }
