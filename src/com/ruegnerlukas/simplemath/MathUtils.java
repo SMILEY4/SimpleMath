4e99bea1f65e3bcb40ee50687dd3750663bfa05e
Lukas Rügner <ruegnerlukas@gmail.com>
2017-10-28 21:38:38 +0200

    Comment MathUtils

--- src/com/ruegnerlukas/simplemath/MathUtils.java
+++ src/com/ruegnerlukas/simplemath/MathUtils.java
@@ -5,8 +5,12 @@ package com.ruegnerlukas.simplemath;
 public class MathUtils {
 
 	
+	
+	
 	private static final float EPSILON = 0.000001f;
 
+	
+	
 	
 	
 	
@@ -17,11 +21,20 @@ public class MathUtils {
 	
 	
 	
+	
+	/**
+	 * @return true, if a is nearly equal to b.
+	 * */
 	public static boolean isNearlyEqual(float a, float b) {
 		return isNearlyEqual(a, b, false);
 	}
 	
 	
+	
+	
+	/**
+	 * @return true, if a is nearly equal to b. Set "dynamicEpsilon" to true, to calculate the threshold for each pair of values.
+	 * */
 	public static boolean isNearlyEqual(float a, float b, boolean dynamicEpsilon) {
 		if(dynamicEpsilon) {
 			return isNearlyEqual(a, b, Math.max(Math.ulp(a), Math.ulp(b)));
@@ -31,6 +44,11 @@ public class MathUtils {
 	}
 	
 	
+	
+	
+	/**
+	 * @return true, if a is nearly equal to b. Uses the given epsilon as a threshold
+	 * */
 	public static boolean isNearlyEqual(float a, float b, float epsilon) {
 		return Math.abs(a-b) < epsilon;
 	}
@@ -38,11 +56,19 @@ public class MathUtils {
 	
 	
 	
+	/**
+	 * @return true, if a is nearly equal to b.
+	 * */
 	public static boolean isNearlyEqual(double a, double b) {
 		return isNearlyEqual(a, b, false);
 	}
 	
 	
+	
+	
+	/**
+	 * @return true, if a is nearly equal to b. Set "dynamicEpsilon" to true, to calculate the threshold for each pair of values.
+	 * */
 	public static boolean isNearlyEqual(double a, double b, boolean dynamicEpsilon) {
 		if(dynamicEpsilon) {
 			return isNearlyEqual(a, b, Math.max(Math.ulp(a), Math.ulp(b)));
@@ -52,6 +78,11 @@ public class MathUtils {
 	}
 	
 	
+	
+	
+	/**
+	 * @return true, if a is nearly equal to b. Uses the given epsilon as a threshold
+	 * */
 	public static boolean isNearlyEqual(double a, double b, double epsilon) {
 		return Math.abs(a-b) < epsilon;
 	}
@@ -59,6 +90,9 @@ public class MathUtils {
 	
 	
 	
+	/**
+	 * limits the given value v to the given minimum / maximum value
+	 * */
 	public static short clamp(short v, short min, short max) {
 		return (short) Math.min(Math.max(v, min), max);
 	}
@@ -66,6 +100,9 @@ public class MathUtils {
 	
 	
 	
+	/**
+	 * limits the given value v to the given minimum / maximum value
+	 * */
 	public static int clamp(int v, int min, int max) {
 		return Math.min(Math.max(v, min), max);
 	}
@@ -73,6 +110,9 @@ public class MathUtils {
 	
 	
 	
+	/**
+	 * limits the given value v to the given minimum / maximum value
+	 * */
 	public static long clamp(long v, long min, long max) {
 		return Math.min(Math.max(v, min), max);
 	}
@@ -80,6 +120,9 @@ public class MathUtils {
 	
 	
 	
+	/**
+	 * limits the given value v to the given minimum / maximum value
+	 * */
 	public static float clamp(float v, float min, float max) {
 		return Math.min(Math.max(v, min), max);
 	}
@@ -87,6 +130,9 @@ public class MathUtils {
 	
 	
 	
+	/**
+	 * limits the given value v to the given minimum / maximum value
+	 * */
 	public static double clamp(double v, double min, double max) {
 		return Math.min(Math.max(v, min), max);
 	}
